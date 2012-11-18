9d7d2aefbee02dd28988959c368f856b2c3f36ff
infidel <aval57@yahoo.com>
2012-11-18 17:42:45 -0500

    bump version

--- xbmc/xbmcinit.py
+++ xbmc/xbmcinit.py
@@ -4,7 +4,7 @@ from xml.etree.ElementTree import ElementTree, fromstring
 from xml.dom.minidom import parseString
 from xml.sax.saxutils import unescape
 
-version = '0.3.1'
+version = '0.3.2'
 
 # see http://wiki.xbmc.org/index.php?title=Special_protocol
 try: _special
